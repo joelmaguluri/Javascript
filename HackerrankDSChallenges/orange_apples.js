5806d04cc9977837c3dbd5aebb9380150efc3768
sudeep745 <msudeep.joel@gmail.com>
2019-02-10 18:23:47 +0530

    apple_orange

---
+++ HackerrankDSChallenges/orange_apples.js
@@ -0,0 +1,77 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countApplesAndOranges function below.
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    var apple_count=0, orange_count=0;
+    for (var i = 0; i < apples.length; i++)
+    {
+        apples[i] = apples[i] + a;
+
+    }
+    for (var i = 0; i < apples.length; i++) {
+        if (apples[i] >= s && apples[i] <= t)
+            apple_count++;
+
+    }
+    for (var i = 0; i < oranges.length; i++) {
+        oranges[i] = oranges[i] + b;
+
+    }
+    for (var i = 0; i < oranges.length; i++) {
+        if (oranges[i] >= s && oranges[i] <= t)
+             orange_count++;
+
+    }
+    console.log(apple_count);
+    console.log(orange_count);
+ 
+}
+
+function main() {
+    const st = readLine().split(' ');
+
+    const s = parseInt(st[0], 10);
+
+    const t = parseInt(st[1], 10);
+
+    const ab = readLine().split(' ');
+
+    const a = parseInt(ab[0], 10);
+
+    const b = parseInt(ab[1], 10);
+
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const apples = readLine().split(' ').map(applesTemp => parseInt(applesTemp, 10));
+
+    const oranges = readLine().split(' ').map(orangesTemp => parseInt(orangesTemp, 10));
+
+    countApplesAndOranges(s, t, a, b, apples, oranges);
+}
+
